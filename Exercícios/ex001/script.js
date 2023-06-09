9d55bb88f065ab50a0c5285160fc10d57800a560
Alexandre <jalexandre.caldeiraa@gmail.com>
2023-06-08 22:47:39 -0300

    Update ex001
    Implementação dos códigos do ex001 - Ainda não finalizado

--- Exercícios/ex001/script.js
+++ Exercícios/ex001/script.js
@@ -1,1 +1,10 @@
-alert('Olá')
+var body = window.document.querySelector('body')
+body.addEventListener('load', carregar)
+
+function carregar(){
+    var msg = window.document.querySelector('div#msg')
+    var foto = window.document.querySelector('img#img')
+    var hora = new Date().getHours()
+
+    msg.innerHTML = `Agora são ${hora}h`
+}
