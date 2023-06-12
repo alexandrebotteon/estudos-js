cc6b83b2c8b36059532429acb88c6ae49d856277
Alexandre <jalexandre.caldeiraa@gmail.com>
2023-06-11 23:21:59 -0300

    Update script.js
    Implementação da verificação da hora

--- Exercícios/ex001/script.js
+++ Exercícios/ex001/script.js
@@ -1,10 +1,18 @@
-var body = window.document.querySelector('body')
-body.addEventListener('load', carregar)
-
 function carregar(){
     var msg = window.document.querySelector('div#msg')
     var foto = window.document.querySelector('img#img')
     var hora = new Date().getHours()
 
     msg.innerHTML = `Agora são ${hora}h`
+
+    if (hora >= 0 && hora < 12){
+        foto.src = 'img/manha.jpg'
+        window.document.body.style.backgroundColor = '#e2cd9f'
+    } else if(hora >= 12 && hora < 18){
+        foto.src = 'img/tarde.jpg'
+        window.document.body.style.backgroundColor = '#b9846f'
+    } else {
+        foto.src = 'img/noite.jpg'
+        window.document.body.style.backgroundColor = '#515154'
+    }
 }
