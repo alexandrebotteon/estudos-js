865f889914533df652b5c3226b17263627a28053
Alexandre <jalexandre.caldeiraa@gmail.com>
2023-06-12 23:43:37 -0300

    Update script.js
    Verificação das idades

--- Exercícios/ex002/script.js
+++ Exercícios/ex002/script.js
@@ -11,12 +11,37 @@ function verificar(){
     } else{
         var idade = ano - nasc
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
         if (document.getElementById('masc').checked){
-            genero = 'Masculino'
+            genero = 'Homem'
+            
+            if (idade >= 1 && idade <= 10){
+                
+            } else if (idade <= 20){
+
+            } else if (idade <= 50){
+
+            } else {
+
+            }
         } else if (document.getElementById('fem').checked) {
-            genero = 'Feminino'
+            genero = 'Mulher'
+
+            if (idade >= 1 && idade <= 10){
+
+            } else if (idade <= 20){
+
+            } else if (idade <= 50){
+
+            } else {
+                
+            }
         }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} de ${idade} anos`
     }
 
 }
