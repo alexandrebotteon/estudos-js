169552f32265eb6aec46717bba41a2c776fbba22
Alexandre <jalexandre.caldeiraa@gmail.com>
2023-06-12 23:51:23 -0300

    Update ex002
    Implementação da apresentação de imagens

--- Exercícios/ex002/script.js
+++ Exercícios/ex002/script.js
@@ -12,36 +12,37 @@ function verificar(){
         var idade = ano - nasc
         var genero = ''
         var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+        img.setAttribute('id', 'img')
 
         if (document.getElementById('masc').checked){
             genero = 'Homem'
             
             if (idade >= 1 && idade <= 10){
-                
+                img.setAttribute('src', 'img/bebe_h.jpg')
             } else if (idade <= 20){
-
+                img.setAttribute('src', 'img/jovem_h.jpg')
             } else if (idade <= 50){
-
+                img.setAttribute('src', 'img/adulto_h.jpg')
             } else {
-
+                img.setAttribute('src', 'img/idoso.jpg')
             }
         } else if (document.getElementById('fem').checked) {
             genero = 'Mulher'
 
             if (idade >= 1 && idade <= 10){
-
+                img.setAttribute('src', 'img/bebe_m.jpg')
             } else if (idade <= 20){
-
+                img.setAttribute('src', 'img/jovem_m.jpg')
             } else if (idade <= 50){
-
+                img.setAttribute('src', 'img/adulto_m.jpg')
             } else {
-                
+                img.setAttribute('src', 'img/idosa.jpg')
             }
         }
 
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} de ${idade} anos`
+        res.appendChild(img)
     }
 
 }
